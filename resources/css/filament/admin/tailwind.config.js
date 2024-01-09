7f7e3ed1930f730f2fa2803aec5dd469e6a51f1d
JobiJoba <Johann.baziret@gmail.com>
2024-01-09 07:51:00 +0700

    fix css issue

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     content: [
         "./app/Filament/**/*.php",
         "./resources/views/filament/**/*.blade.php",
-        "./vendor/filament/**/*.blade.php",
         "./vendor/awcodes/filament-curator/resources/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
     ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
+        "./vendor/awcodes/filament-curator/resources/**/*.blade.php",
         "./resources/views/**/*.blade.php",
     ],
 
